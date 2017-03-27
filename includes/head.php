9344764ddb5641d591b14a3cb41547e481852efd
Rishabh Anand <ranand16@gmail.com>
2017-03-27 16:21:51 +0530

    2nd commit

---
+++ includes/head.php
@@ -0,0 +1,12 @@
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.5">
+  <meta charset="utf-8">
+  <title></title>
+    <link rel="stylesheet" href="css/footer.css" type="text/css">
+    <link rel="stylesheet" href="css/common.css" type="text/css">
+    <!-- <script src="https://use.fontawesome.com/46a3c72b16.js"></script> -->
+    <link rel="stylesheet" href="css/bootstrap-3.3.7-dist/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css" type="text/css">
+
+    <script src="css/jquery-3.2.0.min.js"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+  <?php include("includes/head.php"); ?>
+  <body>
+    <?php include("includes/header.php"); ?>
+
+    <div class="contain">
+      <div class="experiment">
+        <div id="box1" class="box">
+        </div>
+        <div id="box2" class="box">
+        </div>
+      </div>
+      <div class="logo">
+        <div class="logoLetter">
+          <div id="Tmain1" class="t">
+            <div id="Tmain11" class="">
+
+            </div>
+            <div id="Tmain12" class="">
+
+            </div>
+            <div id="Tmain13" class="">
+
+            </div>
+
+          </div>
+          <div id="Tmain2" class="t">
+            <div id="Tmain21" class="">
+
+            </div>
+            <div id="Tmain22" class="">
+
+            </div>
+            <div id="Tmain23" class="">
+
+            </div>
+
+          </div>
+        </div>
+        <div class="logoLetter">
+          <div id="Hmain1" class="l">
+            <div id="Hmain11" class="">
+
+            </div>
+            <div id="Hmain12" class="">
+
+            </div>
+            <div id="Hmain13" class="">
+
+            </div>
+
+          </div>
+          <div id="Hmain2" class="h">
+            <div id="Hmain21" class="">
+
+            </div>
+            <div id="Hmain22" class="">
+
+            </div>
+            <div id="Hmain23" class="">
+
+            </div>
+
+          </div>
+          <div id="Hmain3" class="h">
+            <div id="Hmain31" class="">
+
+            </div>
+            <div id="Hmain32" class="">
+
+            </div>
+            <div id="Hmain33" class="">
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <?php include("includes/footer.php"); ?>
+  </body>
+</html>
