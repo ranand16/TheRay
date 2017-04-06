c72183bfb114a189193d4d0a30834a05105f44c1
Rishabh Anand <ranand16@gmail.com>
2017-04-06 18:16:01 +0530

    060417

--- includes/head.php
+++ includes/head.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.5">
   <meta charset="utf-8">
@@ -9,3 +11,5 @@
     <link rel="stylesheet" href="css/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css" type="text/css">
     <script src="css/jquery-3.2.0.min.js"></script>
 </head>
+<body>
+  <div class="">
